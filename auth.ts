dc05030a98f454298a51daaaadffecb07421468e
lanzejun <lanzejun@cn.ibm.com>
2024-01-08 16:53:06 +0800

    update

--- auth.ts
+++ auth.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
-import bcrypt from 'bcrypt';
 import { db } from '@/lib/db';
 import { User } from '@prisma/client';
 
@@ -34,10 +33,7 @@ export const { auth, signIn, signOut } = NextAuth({
           const user = await getUser(email);
           if (!user) return null;
 
-          const hashedPassword = await bcrypt.hash("abc123_", 10);
-          const passwordsMatch = await bcrypt.compare(password, hashedPassword);
-
-          if (passwordsMatch) return user;
+          return user;
         }
         console.log('Invalid credentials');
         return null;
